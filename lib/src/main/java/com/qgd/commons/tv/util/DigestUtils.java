2f973e642370d3fd4c055f8960fcf814be554135
chentianyi <ethan.chen@fm2020.com>
2016-02-25 19:50:32 +0800

    [IMP]新增md5校验

--- lib/src/main/java/com/qgd/commons/tv/util/DigestUtils.java
+++ lib/src/main/java/com/qgd/commons/tv/util/DigestUtils.java
@@ -1,10 +1,12 @@
 package com.qgd.commons.tv.util;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.security.MessageDigest;
 
 /**
  * DigestUtils
- * 
+ *
  * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2014-03-20
  */
 public class DigestUtils {
@@ -12,8 +14,7 @@ public class DigestUtils {
     /**
      * Used to build output as Hex
      */
-    private static final char[] DIGITS_LOWER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
-            'e', 'f'                         };
+    private static final char[] DIGITS_LOWER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     private DigestUtils() {
         throw new AssertionError();
@@ -21,7 +22,7 @@ public class DigestUtils {
 
     /**
      * encode By MD5
-     * 
+     *
      * @param str
      * @return String
      */
@@ -42,7 +43,7 @@ public class DigestUtils {
      * Converts an array of bytes into an array of characters representing the hexadecimal values of each byte in order.
      * The returned array will be double the length of the passed array, as it takes two characters to represent any
      * given byte.
-     * 
+     *
      * @param data a byte[] to convert to Hex characters
      * @return A char[] containing hexadecimal characters
      */
@@ -56,4 +57,33 @@ public class DigestUtils {
         }
         return out;
     }
+
+    public static String toHexString(byte[] b) {
+        StringBuilder sb = new StringBuilder(b.length * 2);
+        for (int i = 0; i < b.length; i++) {
+            sb.append(DIGITS_LOWER[(b[i] & 0xf0) >>> 4]);
+            sb.append(DIGITS_LOWER[b[i] & 0x0f]);
+        }
+        return sb.toString();
+    }
+
+    public static String md5sum(String filename) {
+        InputStream fis;
+        byte[] buffer = new byte[1024];
+        int numRead = 0;
+        MessageDigest md5;
+        try {
+            fis = new FileInputStream(filename);
+            md5 = MessageDigest.getInstance("MD5");
+            while ((numRead = fis.read(buffer)) > 0) {
+                md5.update(buffer, 0, numRead);
+            }
+            fis.close();
+            return toHexString(md5.digest());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
