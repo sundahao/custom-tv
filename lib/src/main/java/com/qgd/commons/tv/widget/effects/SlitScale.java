585c42f6702b26796ecd32321edba019215d201f
chentianyi <ethan.chen@fm2020.com>
2016-06-27 14:13:24 +0800

    [IMP]新增对话框动画效果。

--- lib/src/main/java/com/qgd/commons/tv/widget/effects/SlitScale.java
+++ lib/src/main/java/com/qgd/commons/tv/widget/effects/SlitScale.java
@@ -1,6 +1,7 @@
 package com.qgd.commons.tv.widget.effects;
 
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 
 import com.qgd.commons.tv.widget.animation.ObjectAnimator;
 
@@ -8,11 +9,15 @@ public class SlitScale extends BaseEffects{
 
     @Override
     protected void setupAnimation(View view) {
+        mDuration=700;
+
         getAnimatorSet().playTogether(
-                ObjectAnimator.ofFloat(view, "rotationY", 90,88,88,45,0).setDuration(mDuration),
+                ObjectAnimator.ofFloat(view, "rotationY", 120,88,68,45,0).setDuration(mDuration),
                 ObjectAnimator.ofFloat(view, "alpha", 0,0.4f,0.8f, 1).setDuration(mDuration),
-                ObjectAnimator.ofFloat(view, "scaleX", 0,0.9f, 0.1f, 0.9f, 1).setDuration(mDuration),
-                ObjectAnimator.ofFloat(view,"scaleY",0,0.9f, 0.1f, 0.9f, 1).setDuration(mDuration)
+                ObjectAnimator.ofFloat(view, "scaleX", 0.3f, 1).setDuration(mDuration),
+                ObjectAnimator.ofFloat(view,"scaleY",0.3f, 1).setDuration(mDuration)
         );
-    }
+        getAnimatorSet().setInterpolator( new AccelerateInterpolator());
+
+     }
 }
