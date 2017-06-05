ae9026ea9f306eeb44e2793ce7b52c43c9bb138c
chentianyi <ethan.chen@fm2020.com>
2017-06-05 11:08:23 +0800

    [IMP]按钮圆角角度可以覆盖

---
+++ sample/src/main/java/test/qgd/com/test/TestStyleActivity.java
@@ -0,0 +1,17 @@
+package test.qgd.com.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 作者：ethan on 2017/5/10
+ * 邮箱：ethan.chen@fm2020.com
+ */
+public class TestStyleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_style);
+
+    }
+}
